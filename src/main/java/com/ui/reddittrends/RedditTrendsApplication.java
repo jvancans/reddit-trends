28ba72c903e3cf33a4d8abcba4000849d50b242c
Janis Vancans <janis.vancans@live.com>
2019-07-18 20:09:50 +0300

    Init project

---
+++ src/main/java/com/ui/reddittrends/RedditTrendsApplication.java
@@ -0,0 +1,13 @@
+package com.ui.reddittrends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditTrendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditTrendsApplication.class, args);
+	}
+
+}
